c057303b12ea85309a3bb091f6a74c690a5d9500
Joshua Utter <jutter18@mail.wou.edu>
2021-04-12 15:06:50 -0700

    Fadded nutrition (#80)
    * [feature] Added nutrional information to db context and models
    
    * [incomplete] Working on saving ingredient nutritional information on search.
    
    * [update] Ingredient details now save basic nutrition info.
    
    * [update] working on saving nutrition info for recipes
    
    * [refactor] rescaffolded Models and finished the up script
    
    * [Update] Added diet table, added meal types to recipes
    
    * [Update] Actually added diet table
    
    * [bug] JS isn't working locally for me
    
    * [Feature] now displaying amounts and tracking nutrition on a per serving basis.(Nutrition currently not displayed)
    
    * [fix] Fixed storing recipe info
    
    * [fix] Fixed Recipe details saving to db
    
    * [fix] added partials

---
+++ MealFridge/Models/Partials.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealFridge.Models
+{
+    public partial class Recipe
+    {
+        public void UpdateRecipe(Recipe r)
+        {
+            Servings = r.Servings;
+            Minutes = r.Minutes;
+            Summery = r.Summery;
+            Instructions = r.Instructions;
+            Cost = r.Cost;
+            ServingSize = r.ServingSize;
+            ServingUnit = r.ServingUnit;
+            Calories = r.Calories;
+            TotalFat = r.TotalFat;
+            SatFat = r.SatFat;
+            Carbs = r.Carbs;
+            NetCarbs = r.NetCarbs;
+            Sugar = r.Sugar;
+            Cholesterol = r.Cholesterol;
+            Sodium = r.Sodium;
+            Protein = r.Protein;
+            Breakfast = r.Breakfast;
+            Lunch = r.Lunch;
+            Dinner = r.Dinner;
+            Dessert = r.Dessert;
+            Snack = r.Snack;
+        }
+    }
+}
