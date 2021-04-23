baaf59c4f58b5c818692736854bc3e137ce4d6f5
cmorris19 <56059629+cmorris19@users.noreply.github.com>
2021-04-22 19:45:52 -0700

    Got the basics for deleting a favorited recipe

--- MealFridge/Controllers/AccountManagementController.cs
+++ MealFridge/Controllers/AccountManagementController.cs
@@ -2,15 +2,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MealFridge.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using MealFridge.Utils;
 
 
 namespace MealFridge.Controllers
 {
     public class AccountManagementController : Controller
     {
-        // GET: AccountManagement
+        private readonly IConfiguration _configuration;
+        private readonly MealFridgeDbContext _context;
+        private readonly UserManager<IdentityUser> _user;
 
-        public ActionResult Index()
+        public AccountManagementController(IConfiguration config, MealFridgeDbContext context, UserManager<IdentityUser> user)
+        {
+            _configuration = config;
+            _context = context;
+            _user = user;
+        }
+
+        public async Task<ActionResult> Index()
         {
             return View();
         }
@@ -22,9 +42,29 @@ namespace MealFridge.Controllers
         {
             return View();
         }
-        public ActionResult FavoriteRecipes()
+        public async Task<ActionResult> FavoriteRecipes()
         {
-            return View();
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = _user.GetUserId(User);
+                var userSavedRecipes = _context.Savedrecipes.Where(f => f.AccountId == userId).Include(r => r.Recipe).ToList();
+                return await Task.FromResult(View("FavoriteRecipes", userSavedRecipes.ToList()));
+            }
+            return await Task.FromResult(RedirectToAction("Index", "Home"));
+        }
+
+        public async Task<ActionResult> Delete(string account_id, int recipe_id)
+        {
+            var temp = _context.Savedrecipes.Where(a => a.AccountId == account_id).ToList();
+            foreach(var i in temp)
+            {
+                if (i.RecipeId == recipe_id)
+                {
+                    _context.Savedrecipes.Remove(i);
+                }
+            }
+            _context.SaveChanges();
+            return await Task.FromResult(RedirectToAction("FavoriteRecipes"));
         }
     }
 }
