e38a3f2aae1f1763bf7152b1f3386cc5c673aad4
Christian Morris <cmorris19@wou.edu>
2021-04-24 12:41:29 -0700

    Branch b adds SavedRecipe Repo + ISavedRecipe repo. The account management view FavoriteRecipes still has an error (#92)
    * Add files via upload
    
    * Finished Restrictions + SavedRecipes
    
    * Finished Testing both Restrictions and Savedrecipes
    
    * Fixed up script
    
    * Got the basics for deleting a favorited recipe
    
    * Functionality of stories done without repo
    
    * THERE ARE ERRORS IN THE VIEWS, this is just so that my SavedRecipe Repo is checked in
    
    Co-authored-by: Joshua Utter <jutter18@mail.wou.edu>

---
+++ MealFridge/Models/Repositories/SavedrecipeRepo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using MealFridge.Models.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MealFridge.Models;
+using MealFridge.Models.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+using MealFridge.Utils;
+
+
+namespace MealFridge.Models.Repositories
+{
+    public class SavedrecipeRepo : Repository<Savedrecipe>, ISavedrecipeRepo
+    {
+        public SavedrecipeRepo(MealFridgeDbContext ctx) : base(ctx) { }
+
+      public List<Savedrecipe> FindAccount(string userId)
+        {
+            var temp = _dbSet.Where(a => a.AccountId == userId).ToList();
+            return temp;
+        }
+        
+
+        public void RemoveSavedRecipe(Savedrecipe recipe)
+        {
+            _context.Remove(recipe);
+            _context.SaveChanges();
+        }
+        public Savedrecipe Savedrecipe(string userId, int recipeId)
+        {
+            return _dbSet.Where(r => r.AccountId == userId && r.RecipeId == recipeId).FirstOrDefault();
+        }
+        public List<Savedrecipe> GetShelvedRecipe(string userId)
+        {
+            IQueryable<Savedrecipe> temp = GetAll();
+            return temp.Where(a => a.AccountId == userId && a.Shelved == true).ToList();
+        }
+        public List<Savedrecipe> GetFavoritedRecipe(string userId)
+        {
+            IQueryable<Savedrecipe> temp = GetAll();
+            return temp.Where(a => a.AccountId == userId && a.Favorited == true).Include(r => r.Recipe).ToList();
+        }
+        //public List<Recipe> GetRecipes(int recipeId)
+        //{
+        //    IQueryable<Recipe> temp = (IQueryable<Recipe>)GetAll();
+        //    return temp.ToList();
+        //}
+    }
+}
+
+        
