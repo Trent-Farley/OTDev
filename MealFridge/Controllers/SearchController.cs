abe33655a774b48b8cb0b6841e616e635d6f3a55
chrisedwou <cedwards19@mail.wou.edu>
2021-05-22 17:20:10 -0700

    Fixed the cheap meals to better match the story written

--- MealFridge/Controllers/SearchController.cs
+++ MealFridge/Controllers/SearchController.cs
@@ -111,7 +111,7 @@ namespace MealFridge.Controllers
             }
             if (query.Cheap)
             {
-                possibleRecipes.OrderBy(r => r.Cost);
+                return await Task.FromResult(PartialView("RecipeCards", possibleRecipes.Where(r=> r.Cost != null).Distinct().Take(10).OrderBy(r => r.Cost)));
             }
             return await Task.FromResult(PartialView("RecipeCards", possibleRecipes.Distinct().Take(10)));
         }
@@ -153,7 +153,7 @@ namespace MealFridge.Controllers
             }
             if(query.Cheap)
             {
-                possibleRecipes.OrderBy(r => r.Cost);
+                return await Task.FromResult(PartialView("RecipeCards", possibleRecipes.Distinct().Take(10).OrderBy(r=> r.Cost)));
             }
             return await Task.FromResult(PartialView("RecipeCards", possibleRecipes.Distinct().Take(10)));
         }
